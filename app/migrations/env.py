cc04ade78fbcc77d6328c86c7526c206b10ce4be
sane4ek24 <119444641+sane4ek24@users.noreply.github.com>
2024-11-22 02:34:28 +0500

    module_17.py commit

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -18,8 +18,10 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from app.backend import Base
-target_metadata = None
+from app.backend.db import Base
+from app.models.user import User
+from app.models.task import Task
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
